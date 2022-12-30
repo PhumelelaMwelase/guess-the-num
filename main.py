52a4f47a66ca74f96b49dedcf486419582cf4465
phumelelamwelase <site.phumelela@gmail.com>
2022-12-30 12:53:57 +0200

    Fix bug that causes program to not recognize correct guess when life is 1 Add up and down emojis to make it easier to keep track of guesses

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ high_responses = ["While admirable, generosity isn't always warranted!", "Much t
 low_responses = ["This is no time to be coy!", "Stinginess is not an attractive characteristic.",
                  "A stingy one."]
 
+
+
 @app.route("/")
 def home():
     # Footer #
@@ -32,30 +34,37 @@ def home():
 @app.route("/", methods=["POST"])
 def receive_data():
     global lives, random_number, wins, high_score
+
     guess = int(request.form["number"])
+
     if wins > high_score:
         high_score = wins
-    while lives > 1:
-        if guess == random_number:
-            answer = f"You Got It. The correct answer was {random_number}.\nEnter a new number to keep playing."
-            wins += 1
-            lives = 10 - wins
-            random_number = random.randint(1, 100)
-        elif guess < random_number:
-            lives -= 1
-            answer = f"{random.choice(low_responses)} Go higher."
+
+    if guess == random_number:
+        answer = f"You Got It. The correct answer was {random_number}.\nEnter a new number to keep playing ✅"
+        wins += 1
+        lives = 10 - wins
+        random_number = random.randint(1, 100)
+        return render_template("index.html", num_lives=lives, comment=answer, wins=wins, highest=high_score)
+
+    else:
+        while lives > 1:
+            if guess < random_number:
+                lives -= 1
+                answer = f"{random.choice(low_responses)} Go higher ⬆️"
+
+            else:
+                lives -= 1
+                answer = f"{random.choice(high_responses)} Go lower ⬇️"
+
+            return render_template("index.html", num_lives=lives, comment=answer, wins=wins, highest=high_score)
         else:
-            lives -= 1
-            answer = f"{random.choice(high_responses)} Go lower."
+            answer = f"You lose! The correct answer was {random_number}.\nEnter a new number to keep playing."
+            lives = 10
+            random_number = random.randint(1, 100)
+            wins = 0
 
         return render_template("index.html", num_lives=lives, comment=answer, wins=wins, highest=high_score)
-    else:
-        answer = f"You lose! The correct answer was {random_number}.\nEnter a new number to keep playing."
-        lives = 10
-        random_number = random.randint(1, 100)
-        wins = 0
-    return render_template("index.html", num_lives=lives, comment=answer, wins=wins, highest=high_score)
-
 
 
 if __name__ == "__main__":
